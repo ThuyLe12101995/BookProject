2ba0d390f1029c283b8c9d57f655900dcf1927e2
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-02-23 11:11:20 +0700

    version 1.0

---
+++ HandlerData.js
@@ -0,0 +1,53 @@
+const mysql = require('mysql');
+
+class handlerData {
+
+    constructor(config) {
+        this.conn=mysql.createConnection(config);
+    }
+
+    GetBooks(sql) {
+        let conn = this.conn;
+        return new Promise(function (resolve,reject) {
+            conn.query(sql,function (err,result) {
+                if (err)
+                    reject(err);
+                resolve(result);
+            });
+        });
+    }
+
+    SearBook(sql,id) {
+        let conn=this.conn;
+        return new Promise(function (resolve,reject) {
+           conn.query(sql,id,function (err,result) {
+               if (err)
+                   reject(err);
+               resolve(result);
+           }) ;
+        });
+    }
+    DeleteBook(sql,id) {
+        let conn=this.conn;
+        return new Promise(function (resolve,reject) {
+           conn.query(sql,id,function (err,result) {
+               if (err)
+                   reject(err);
+               resolve(result);
+           })
+        });
+    }
+    CreateBook(sql,book) {
+        let conn = this.conn;
+        return new Promise(function (resolve,reject) {
+            conn.query(sql,book,function (err,result) {
+                if (err)
+                    reject(err);
+                resolve(result);
+            });
+        });
+    }
+
+}
+
+module.exports=handlerData;
---
+++ index.js
@@ -0,0 +1,48 @@
+const express =  require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const config = require('./config');
+const handlerData = require('./HandlerData');
+//Middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+let handler = new handlerData(config);
+
+let getBook = 'select * from book';
+let searBook = 'select * from book where id = ?';
+let createBook= 'insert into book set ?';
+let deleteBook = 'delete from book where id = ?';
+app.get('/books',function (req,res) {
+    handler.GetBooks(getBook).then(function (result) {
+        res.send(result);
+    });
+});
+
+app.get('/books/:id',function (req,res) {
+    handler.SearBook(searBook,req.params.id).then(function (result) {
+        res.json(result);
+    });
+});
+
+app.delete('/books/:id',function (req,res) {
+    handler.DeleteBook(deleteBook,req.params.id).then(function (result) {
+        res.status(200).send(result);
+    });
+});
+
+app.post('/books',function (req,res) {
+    handler.CreateBook(createBook,req.body).then(function (result) {
+       res.send({state:"success",
+                    ID:result.insertId
+       });
+    });
+});
+
+app.put('/books/:id',function (req,res) {
+
+});
+
+app.listen(3000,function () {
+   console.log("Server running in port 3000 !");
+});
