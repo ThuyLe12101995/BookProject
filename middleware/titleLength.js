b31471956902cb21128aa3348f1e235d634eef8d
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-02-26 16:28:56 +0700

    add titlelength

--- middleware/titleLength.js
+++ middleware/titleLength.js
@@ -1,5 +1,5 @@
 module.exports =function (req,res,next) {
-  if (req.body.title > 40 || req.body.author > 100) {
+  if (req.body.title.length > 40 || req.body.author.length > 100) {
       return res.status(400).send({message : 'title length must less 40 and  author must less 100'});
   }
   next();
