be992d223c31c26fea4438ff70765b73474d9eb8
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-04 22:42:29 +0700

    check title must not null

--- middleware/check-title-null.js
+++ middleware/check-title-null.js
@@ -1,6 +1,6 @@
 module.exports = function (request, response, next) {
     if (!request.body.title || !request.body.author) {
-       return response.status(400).json({message:"title or author must not null"});
+        return response.status(400).json({message:"title or author must not null"});
     }
     next();
 };
