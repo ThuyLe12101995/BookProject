e87c1fa0b458b9e99fe20b9a6cf3aa034e8cc7ad
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-04 15:41:50 +0700

    convert to class

--- controllers/bookcontroller.js
+++ controllers/bookcontroller.js
@@ -1,36 +1,11 @@
-const handlerData = require('../models');
-const config = require('../mysql-config');
-let handler = new handlerData(config);
+const HandlerData   = require('../handler-data');
+const mysqlConfig   = require('../mysql-config');
 
-exports.book_list = function (req, res) {
-    handler.GetBooks().then(function (result) {
-        res.send(result);
-    });
-};
+let handlerData   = new HandlerData(mysqlConfig);
 
-exports.search_book = function (req, res) {
-    handler.SearBook(req.params.id).then(function (result) {
-        res.send(result);
-    });
-};
+class BookController {
 
-exports.update_book = function (req, res) {
-    handler.UpdateBook(req.params.id,req.body).then(function (result) {
-        res.status(200).send({alert:'success',message:result.message});
-    });
-};
 
-exports.delete_book = function (req, res) {
-    handler.SoftDelete(req.params.id).then(function (result) {
-        res.status(200).send({alert:"success",message:result.message});
-    });
-};
+}
 
-exports.create_book = function (req, res) {
-    handler.CreateBook(req.body).then(function (result) {
-        res.status(201).send({state:"success",
-            ID:result.insertId
-        });
-    });
-};
-
+module.exports = BookController;
