2f998465020c096e75ac0d2e8dc63a61910c6677
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-03-04 22:26:08 +0700

    check length of titlle

--- middleware/check-title-length.js
+++ middleware/check-title-length.js
@@ -1,6 +1,6 @@
 module.exports = function (request, response, next) {
     if (request.body.title.length > 40 || request.body.author.length > 100) {
-      return response.status(400).json({message : 'title length must < 40 and  author must < 100'});
+        return response.status(400).json({message : 'title length must < 40 and  author must < 100'});
     }
     next();
 };
